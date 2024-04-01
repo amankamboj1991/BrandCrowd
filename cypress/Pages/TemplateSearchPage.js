e5062627e912572a21c25409671dc78c29a6ec81
Aman <amankamboj1207@gmail.com>
2024-04-01 22:12:22 +1100

    Added new test

--- cypress/Pages/TemplateSearchPage.js
+++ cypress/Pages/TemplateSearchPage.js
@@ -3,7 +3,8 @@ export default class TemplateSearchPage{
     webLocators = {
         searchField: '#searchTerm',
         heading: '.tw-font-bold.tw-text-4xl.tw-mb-4.tw-text-grayscale-800',
-        categoryButton: '(//div[@class=\'tw-flex tw-py-2 tw-items-center tw-cursor-pointer\'])[1]'        
+        categoryButton: '(//div[@class=\'tw-flex tw-py-2 tw-items-center tw-cursor-pointer\'])[1]',        
+        upgradeAccountButton: '.tw-hidden.md\\:tw-block.tw-mr-6.tw-my-auto a[href*="upgrade-account"]'
     }
 
         openUrl(){
@@ -16,6 +17,11 @@ export default class TemplateSearchPage{
         
         clickCategory(){            
             cy.xpath(this.webLocators.categoryButton).click()                       
+        }
+        
+        clickAndVerifyUpgradeAccount(){
+          cy.get(this.webLocators.upgradeAccountButton).click() 
+          cy.title().should('eq', 'Checkout | BrandCrowd')         
         }
         
 }
--- cypress/e2e/Tests/TemplateSearchPageTest.cy.js
+++ cypress/e2e/Tests/TemplateSearchPageTest.cy.js
@@ -2,16 +2,28 @@ import TemplateSearchPage from "../../Pages/TemplateSearchPage";
 
 const templateSearchPage = new TemplateSearchPage();
 
-describe("Template Search Page tests", () => {
-  
+describe("Top navigation bar", () => {
+
   it("Verify Page heading", () => {      
       templateSearchPage.openUrl()
       templateSearchPage.pageHeading()
   });
-
-  it("Verify Category clickable", () => {     
+ 
+  it("Verify Upgrade Account", () => {     
     templateSearchPage.openUrl() 
-    templateSearchPage.clickCategory()    
+    templateSearchPage.clickAndVerifyUpgradeAccount()    
+  });
+
+});
+
+describe("Left Panel", () => {
+  
+  it("Verify Category clickable & Instagram only templates are displayed", () => {     
+    templateSearchPage.openUrl()     
+    templateSearchPage.clickCategory();    
+    //This scenario can be extended to:
+    //- reclick category & check Instagram.
+    // Assert only Instagram templates are displayed
   });
 
 });
